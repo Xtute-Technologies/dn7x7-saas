84ea2bb3182fa0b587f6de4e7610a229b54da463
devangspsingh <devangshauryapratapsingh@gmail.com>
2026-01-07 23:39:50 +0530

    removed grayscale from footer nav user

--- frontend/components/nav-user.jsx
+++ frontend/components/nav-user.jsx
@@ -45,7 +45,7 @@ export function NavUser({
             <SidebarMenuButton
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
+              <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.profile_image} alt={user.name} />
                 <AvatarFallback className="rounded-lg">{user.name?.charAt(0)}</AvatarFallback>
               </Avatar>
